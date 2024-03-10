bc602f8f225fe8590b93478864e541b5b38a3361
James Charters <me@jamescharters.info>
2024-03-10 20:53:32 +1100

    feat: add config for vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vitest/config';
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    test: {
+        globals: true,
+        environment: "jsdom",
+        setupFiles: ["./testSetup.ts"],
+        include: ["./src/**/*.test.{ts,tsx}"],
+    },
+    resolve: {
+        alias: {
+            // Allow absolute paths in imports, e.g. import Button from '@/components/Button'
+            "@": "/src",
+        },
+    },
+});
